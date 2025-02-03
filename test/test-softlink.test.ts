11c4a104136a11955c9d78d1cbfb66c91cfee611
Rene Malmgren <rene.malmgren@h3.se>
2025-02-03 13:50:25 +0400

    Bot fix

---
+++ test/test-softlink.test.ts
@@ -0,0 +1,12 @@
+import * as fs from "node:fs";
+import path from "node:path";
+
+describe('MyTest2', () => {
+    before(function () {
+        console.log("MyTest2");
+    })
+
+    it('Lets upload something', async () => {
+        console.log("MyTest2");
+    })
+})
